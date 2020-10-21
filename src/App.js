927eb07e96ea24b1c73802963ed0af2072d5d9fc
Zizohh <jeremiahhussaini2892@gmail.com>
2020-10-21 15:43:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="max-w-sm rounded overflow-hidden shadow-lg">
+			{/* <img className="w-full" src={require("./profile.jpg")} alt="Display" /> */}
+			<div className="px-6 py-4">
+				<div className="font-bold text-green-500 text-xl mb-2">Alt Text</div>
+				<p className="text-gray-700 text-base">Image Details</p>
+			</div>
+			<div className="">
+				<span className="">#Software Engineer</span>
+				<span className="">#Writter</span>
+				<span className="">#Public Speaker</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
